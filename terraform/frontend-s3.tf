5b769c1de8f2d319cdc4a029462989c953fd6225
KarlEtsebeth <148228664+KarlEtsebeth@users.noreply.github.com>
2024-05-02 09:14:42 +0200

    Add infra for frontend and backend

---
+++ terraform/frontend-s3.tf
@@ -0,0 +1,42 @@
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket        = "${local.account-id}-${local.db-name}"
+  force_destroy = true
+}
+
+resource "aws_s3_object" "main" {
+  bucket       = aws_s3_bucket.frontend_bucket.id
+  key          = "main.html"
+  source       = "../frontend/Views/main.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_bucket_ownership_controls" "frontend_bucket" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend_bucket" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+  policy = jsonencode({
+    "Version" : "2008-10-17",
+    "Id" : "PolicyForCloudFrontPrivateContent",
+    "Statement" : [
+      {
+        "Sid" : "AllowCloudFrontServicePrincipal",
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "cloudfront.amazonaws.com"
+        },
+        "Action" : "s3:GetObject",
+        "Resource" : "${aws_s3_bucket.frontend_bucket.arn}/*",
+        "Condition" : {
+          "StringEquals" : {
+            "AWS:SourceArn" : "${aws_cloudfront_distribution.frontend.arn}"
+          }
+        }
+      }
+    ]
+  })
+}
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,4 +2,5 @@ locals {
   # Remember to change account-id in backend.tf - doesn't accept vars
   account-id = "978251882572"
   db-name    = "wordle"
+  cert-arn   = "arn:aws:acm:eu-west-1:978251882572:certificate/f83ffc81-fd57-4c78-8907-0bce5d1f28fc"
 }
