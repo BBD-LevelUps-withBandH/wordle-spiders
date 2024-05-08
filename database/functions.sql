4c0ddc82da517dfe2b4def9edc245946a40644e2
KarlEtsebeth <148228664+KarlEtsebeth@users.noreply.github.com>
2024-05-08 17:17:06 +0200

    Change most calls to use email and word instead of user_id and word_id

--- database/functions.sql
+++ database/functions.sql
@@ -1,6 +1,6 @@
 --changeset karl:function:calc_max_remaining_scores runOnChange:true
 CREATE OR REPLACE FUNCTION calc_max_remaining_scores(
-    client_id INT
+    user_email_in VARCHAR(50)
 )
 RETURNS INT
 AS $$
@@ -15,7 +15,8 @@ BEGIN
 	SELECT COUNT(*) INTO user_scores_of_day
 	FROM scores 
 	INNER JOIN words ON scores.word_id = words.word_id
-	WHERE user_email = client_id AND played_date = CURRENT_DATE;
+    INNER JOIN users ON scores.user_id = users.user_id
+	WHERE user_email = user_email_in AND played_date = CURRENT_DATE;
 
     RETURN num_words_of_day-user_scores_of_day;
 END;
